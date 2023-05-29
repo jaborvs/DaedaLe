da7268bfe390d02174c212ecf561edc97d69a7c9
dennisvet <Dennisvet4@hotmail.com>
2023-05-29 22:57:29 +0200

    Started adding patterns for all possible cells

--- TestZ3.py
+++ TestZ3.py
@@ -1,137 +1,56 @@
 from z3 import *
 
-BACKGROUND = (".", 0)
-WALL = ("#", 1)
-CRATE = ("*", 2)
-PLAYER = ("P", 3)
+# Define the number of timesteps
+num_timesteps = 3
 
-mapping = {
-    BACKGROUND[1]: BACKGROUND[0],
-    WALL[1]: WALL[0],
-    CRATE[1]: CRATE[0],
-    PLAYER[1]: PLAYER[0]
-}
+# Define the size of the grid
+grid_size = 3
 
-class PSObject:
+# Create a list to hold the grids for each timestep
+grids = []
 
-    def __init__(self, name):
-        # self.pos_x = pos_x
-        # self.pos_y = pos_y
-        self.name = name
+# Create a solver
+s = Solver()
 
-class LevelData:
+# Create a grid for each timestep
+for t in range(num_timesteps):
+    # Create a grid
+    grid = [[Int('x_%i_%i_%i' % (t, i, j)) for j in range(grid_size)] for i in range(grid_size)]
+    grids.append(grid)
 
-    def __init__(self, width, height, moving_objects):
-        self.width = width
-        self.height = height
-        self.moving_objects = moving_objects
+    # Add constraints for each cell in the grid
+    for i in range(grid_size):
+        for j in range(grid_size):
+            # If this is the first timestep, add a constraint for the initial pattern
+            if t == 0:
+                if i == 1 and j == 1:
+                    s.add(grid[i][j] == 2)
+                else:
+                    s.add(grid[i][j] == 0)
+            # For subsequent timesteps, add a constraint based on the previous timestep
+            else:
+                # If the cell in the previous grid was 2 and the cells to its right and left were 0,
+                # then the cell in this grid should be 2 and the cells to its right and left should be 0
+                if j > 0 and j < grid_size - 1:
+                    s.add(Implies(And(grids[t-1][i][j-1] == 0, grids[t-1][i][j] == 2, grids[t-1][i][j+1] == 0),
+                                  And(grid[i][j-1] == 2, grid[i][j] == 0, grid[i][j+1] == 0)))
+                # If the cell in the previous grid did not match the pattern, then its value should be the same in this grid
+                if j == 0:
+                    s.add(Implies(grids[t-1][i][j] != 2 or grids[t-1][i][j+1] != 0, grid[i][j] == grids[t-1][i][j]))
+                elif j == grid_size - 1:
+                    s.add(Implies(grids[t-1][i][j] != 2 or grids[t-1][i][j-1] != 0, grid[i][j] == grids[t-1][i][j]))
+                else:
+                    s.add(Implies(grids[t-1][i][j] != 2 or grids[t-1][i][j-1] != 0 or grids[t-1][i][j+1] != 0, grid[i][j] == grids[t-1][i][j]))
 
-ld = LevelData(4,4,2)
-psobjects = [PSObject("Crate"), PSObject("Crate")]
-
-objects = {"Crate": 4, "Player": 1}
-
-# Define the number of objects
-num_objects = 0
-for k in objects:
-    num_objects += objects[k]
-
-distinct_objects = len(mapping)
-
-
-# Define the play area width and height
-playw = 10
-playh = 10
-
-# Create Z3 solver
-solver = Solver()
-
-# Create variables for the object positions
-X = [ [ Int("x_%s_%s" % (i, j)) for j in range(playw) ]
-      for i in range(playh) ]
-
-# Create objects
-# objects = [
-#     PSObject("Crate")
-#     for i in range(num_objects)
-# ]
-
-# Add constraints to ensure objects have distinct positions
-one_object = [ Distinct([ X[i][j] for i in range(playw) ])
-             for j in range(playh) ]
-
-
-for i in range(playw):
-    for j in range(playh):
-        
-        X_val = X[i][j]
-        # Add constraint to enforce the range [0, 5]
-        limit_objects = And(X_val >= 0, X_val < distinct_objects)
-        solver.add(limit_objects)
-        
-        if i == 0 or i == playw - 1 or j == 0 or j == playh - 1:
-            solver.add(X[i][j] == WALL[1])
-        
-
-# crate_objects = Sum([If(X[i][j] == CRATE[1], 1, 0) for i in range(playh) for j in range(playw)])
-# solver.add(crate_objects == objects["Crate"])
-
-# Add constraints for placing a specific number of crates randomly
-positions = []
-for key, value in objects.items():
-    pos_x = Int(f"{key}_x")
-    pos_y = Int(f"{key}_y")
-    positions.append((pos_x, pos_y))
-
-    solver.add(pos_x >= 1, pos_x < playw - 1)
-    solver.add(pos_y >= 1, pos_y < playh - 1)
-    solver.add(Int("x_%s_%s" % (pos_x, pos_y)) == CRATE[1])
-
-# player_objects = Sum([If(X[i][j] == PLAYER[1], 1, 0) for i in range(playh) for j in range(playw)])
-# solver.add(player_objects == objects["Player"])
-
-# background_objects = Sum([If(X[i][j] == BACKGROUND[1], 1, 0) for i in range(playh) for j in range(playw)])
-# solver.add(background_objects == (playw * playh) - (4 * playw - 4) - num_objects)
-
-
-
-# Check if the constraints are satisfiable
-if solver.check() == sat:
-    model = solver.model()
-    # Get the values of the variables
-    objects = [ [ model[X[i][j]].as_long() for i in range(playw) ]
-      for j in range(playh) ]
-
-    print(objects)
-    # object_positions = [(model[X[i][0]].as_long(), model[X[i][0]].as_long()) for i in range(num_objects)]
-    print("Object positions:")
-    for list_objects in objects:
-        row_string = " ".join(mapping[num] for num in list_objects)
-        print(row_string)
-        # print(f"{objects[i].name}: (x={object_positions[i][0]}, y={object_positions[i][1]})")
-
-        # print("\n")
+# Check if there is a solution
+if s.check() == sat:
+    print("Solution found!")
+    m = s.model()
+    for t in range(num_timesteps):
+        for i in range(grid_size):
+            for j in range(grid_size):
+                print(m[grids[t][i][j]], end=' ')
+            print()
+        print()
 else:
-    print("No satisfying assignment.")
-
-
-print ("statistics for the last check method...")
-print (solver.statistics())
-# Traversing statistics
-for k, v in solver.statistics():
-    print ("%s : %s" % (k, v))
-
-# x = Real('x')
-# y = Real('y')
-# s = Solver()
-# s.add(x > 1, y > 1, Or(x + y > 3, x - y < 2))
-# print ("asserted constraints...")
-# for c in s.assertions():
-#     print (c)
-
-# print (s.check())
-# print ("statistics for the last check method...")
-# print (s.statistics())
-# # Traversing statistics
-# for k, v in s.statistics():
-#     print ("%s : %s" % (k, v))
+    print("No solution found.")
--- newTestZ3.py
+++ newTestZ3.py
@@ -1,4 +1,8 @@
 from z3 import *
+import time
+
+timebefore = time.time()
+
 
 BACKGROUND = (".", 0)
 WALL = ("#", 1)
@@ -13,10 +17,11 @@ mapping = {
 }
 
 # Define the dimensions of the level and the number of timesteps
-width = 10
-height = 10
-timesteps = 5
+width = 5
+height = 5
+timesteps = 10
 playerpos = (1, 1)
+winpos = (3, 3)
 
 # Define the 3D array of Z3 integer variables
 level = [[[Int(f"level_{x}_{y}_{t}") for t in range(timesteps)] for y in range(height)] for x in range(width)]
@@ -25,12 +30,10 @@ s = Solver()
 
 distinct_objects = len(mapping)
 
-# Initial setup of the level at timestep 0 (0's and 1's)
+# Initial setup of the level at timestep 0
 for x in range(width):
     for y in range(height):
         cell_val = level[x][y][0]
-        # limit_objects = And(cell_val >= 0, cell_val < distinct_objects)
-        # s.add(limit_objects)
 
         if (x == playerpos[0] and y == playerpos[1]):
             s.add(cell_val == PLAYER[1])
@@ -42,65 +45,99 @@ for x in range(width):
             s.add(cell_val == BACKGROUND[1])
 
 
-pattern = [(0, 0, PLAYER[1]), (1, 0, BACKGROUND[1]), (2, 0, BACKGROUND[1])]
-replacement = [BACKGROUND[1], PLAYER[1], BACKGROUND[1]]
+patterns = []
+replacements = []
+
+move_r = [(0, 0, PLAYER[1]), (1, 0, BACKGROUND[1])]
+patterns.append(move_r)
+repl_move_r = [BACKGROUND[1], PLAYER[1]]
+replacements.append(repl_move_r)
+
+move_l = [(0, 0, PLAYER[1]), (-1, 0, BACKGROUND[1])]
+patterns.append(move_l)
+repl_move_l = [BACKGROUND[1], PLAYER[1]]
+replacements.append(repl_move_l)
+
+move_u = [(0, 0, PLAYER[1]), (0, -1, BACKGROUND[1])]
+patterns.append(move_u)
+repl_move_u = [BACKGROUND[1], PLAYER[1]]
+replacements.append(repl_move_u)
+
+move_d = [(0, 0, PLAYER[1]), (0, 1, BACKGROUND[1])]
+patterns.append(move_d)
+repl_move_d = [BACKGROUND[1], PLAYER[1]]
+replacements.append(repl_move_d)
 
 for t in range(1, timesteps):
 
     all_patterns = []
-    skip = []
 
     new_playerpos = (0,0)
 
-    lhs = []
-    rhs = []
-    unchanged_rhs = []
-    unchanged = []
+    choice_rule = []
 
-    for x in range(0, width):
-        for y in range(0, height):
+    for i in range(len(patterns)):
+        pattern = patterns[i]
 
-            cell_val = level[x][y][t]
-            # limit_objects = (cell_val == level[x][y][t-1])
-            # s.add(limit_objects)
+        # For every cell: Add pattern + changed pattern in next timestep or remain unchanged
+        for m in range(0, width):
+            for n in range(0, height):
 
-            # Check if pattern can be applied at playerpos
-            for i, (xdiff, ydiff, obj) in enumerate(pattern):
+                lhs = []
+                rhs = []
+                unchanged_rhs = []
+                unchanged = []
+                skip = []
 
-                if x + xdiff < width and y + ydiff < height and (x, y) == playerpos:
+                # For every pattern: Go over the whole board.
+                for x in range(0, width):
+                    for y in range(0, height):
 
-                    skip.append(tuple((x + xdiff, y + ydiff)))
+                        # Extract cell value at pos x, y
+                        cell_val = level[x][y][t]
 
-                    lhs.append(level[x + xdiff][y + ydiff][t - 1] == obj)
-                    rhs.append(level[x + xdiff][y + ydiff][t] == replacement[i])
-                    unchanged.append(level[x + xdiff][y + ydiff][t] == level[x + xdiff][y + ydiff][t - 1])
+                        for j, (xdiff, ydiff, obj) in enumerate(pattern):
 
-                    # Remember player obj pos so we can update later
-                    if replacement[i] == PLAYER[1]: 
-                        new_playerpos = tuple((x + xdiff, y + ydiff))       
+                            # Check if pattern fits at player position
+                            if x + xdiff < width and y + ydiff < height:
 
-            if (x,y) in skip:
-                continue
-            else:
-                unchanged_rhs.append(level[x][y][t] == level[x][y][t - 1])
-                    
-            # s.add(Xor(Implies(lhs, And(rhs, unchanged_rhs)), Implies(Not(lhs), And(unchanged_rhs, unchanged))))
+                                skip.append(tuple((x + xdiff, y + ydiff)))
 
-    lhs = And(lhs)
-    rhs = And(rhs)
-    unchanged_rhs = And(unchanged_rhs)
-    unchanged = And(unchanged)
+                                lhs.append(level[x + xdiff][y + ydiff][t - 1] == obj)
+                                unchanged.append(level[x+xdiff][y + ydiff][t] == level[x+xdiff][y + ydiff][t - 1])
 
-    s.add(Xor(And(lhs, rhs, unchanged_rhs), And(Not(lhs), unchanged, unchanged_rhs)))
+                                if ((x + xdiff, y + ydiff) == winpos and t == timesteps - 1):
+                                    rhs.append(level[x][y][t] == PLAYER[1])
+                                else:
+                                    rhs.append(level[x + xdiff][y + ydiff][t] == replacements[i][j])
 
-    print(f"old playerpos = {playerpos}, new playerpos = {new_playerpos}")
-    playerpos = new_playerpos
 
-    # all_patterns.append(Xor(And(lhs, rhs, unchanged_rhs), And(Not(lhs), unchanged, unchanged_rhs)))
-    # s.add(AtLeast(*all_patterns, 1))
+                        if (x,y) in skip:
+                            continue
+                        else:
+                            unchanged_rhs.append(level[x][y][t] == level[x][y][t - 1])
+                                    
+                        # s.add(Xor(Implies(lhs, And(rhs, unchanged_rhs)), Implies(Not(lhs), And(unchanged_rhs, unchanged))))
+
+                lhs = And(lhs)
+                rhs = And(rhs)
+                unchanged_rhs = And(unchanged_rhs)
+                unchanged = And(unchanged)
+
+                # If and else makes unsat for now...
+                if (t != timesteps - 1):
+                    choice_rule.append(Or(And(lhs, rhs, unchanged_rhs), And(Not(lhs), unchanged, unchanged_rhs)))
+                else:
+                    choice_rule.append(And(lhs, rhs, unchanged_rhs))
+
+    # playerpos = new_playerpos
+
+    s.add(AtLeast(*choice_rule, 1))
+
 
 # Now we solve the problem and print the results
 if s.check() == sat:
+    print("sat")
     m = s.model()
     for t in range(timesteps):
         objects = [[m[level[x][y][t]] for x in range(width)] for y in range(height)]
@@ -111,6 +148,5 @@ if s.check() == sat:
             # row_string = " ".join(str(num) if num is not None else "?" for num in list_objects)
             print(row_string)
         print()
-
 else:
     print("Unsat core:", s.unsat_core())
